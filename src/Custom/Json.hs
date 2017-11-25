a2b7947c40f525e2139fb69afab45c4df817f4f9
J. Eduardo V. Morango <edvmorango@gmail.com>
2017-11-24 23:40:57 -0200

    Not-found some error

--- src/Custom/Json.hs
+++ src/Custom/Json.hs
@@ -5,16 +5,32 @@
 {-# LANGUAGE TypeFamilies          #-}
 
 import Yesod
+import Data.Time (getCurrentTime)
 
 data Json = Json
 
 mkYesod "Json" [parseRoutes|
 / HomeR GET
+/error ErrorR GET
+/not-found NotFoundR GET
 |]
 
-instance Yesod Json
+instance Yesod Json where
+  errorHandler NotFound = fmap toTypedContent $ defaultLayout $ do
+    setTitle "Request page not found"
+    toWidget [whamlet| <p> Not found |]
+  errorHandler other = defaultErrorHandler other
 
+getHomeR = do
+  now <- liftIO getCurrentTime
+  setMessage $ toHtml $ "Previously at: " ++ show now
+  defaultLayout [whamlet| <p> Try refresh this page|]
 
-getHomeR = return $ object ["home" .= "Hello Json"]
+getErrorR :: Handler ()
+getErrorR = error "This is a mistake man"
 
+getNotFoundR :: Handler ()
+getNotFoundR = notFound
+
+main :: IO ()
 main = warp 3000 Json
